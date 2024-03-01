d2c0b9f7d591ee8f1da49da84912e69aa1eca886
Madness <97130349+blueMadnessDev@users.noreply.github.com>
2024-03-01 15:33:08 -0500

    update main.html, universalscripts.js and main.html

--- beta/universalscripts.js
+++ beta/universalscripts.js
@@ -1,3 +1,11 @@
+const Corrosion = require('corrosion');
+const proxy = new Corrosion();
+const http = require('http')
+http.createServer((req, res) => 
+  proxy.request(req, res) // Request Proxy
+).on('upgrade', (req, socket, head) => 
+  proxy.upgrade(req, socket, head) // WebSocket Proxy
+).listen(80);
 
 const betaV = localStorage.getItem("betaversion")
 
