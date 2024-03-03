d160b4fc528022ace9803851dcd70e8f9391ed73
Madness <97130349+blueMadnessDev@users.noreply.github.com>
2024-03-03 09:51:45 -0500

    update apps.html, games.html, run.html and getDailyProxyKey.py

--- getDailyProxyKey.py
+++ getDailyProxyKey.py
@@ -9,21 +9,27 @@ try:
     print("Status Code:", response.status_code)
     print("Response Text:", response.text)
 
-    file_path = r"C:\Users\Kiera\OneDrive\Desktop\Programs\Made By Me\b1ueice.github.io\proxykey.txt"
-    with open(file_path, 'w') as file:
-        file.write(response.text)
+    # Define the file paths
+    file_path1 = r"C:\Users\Kiera\OneDrive\Desktop\Programs\Made By Me\b1ueice.github.io\proxykey.txt"
     file_path2 = r"C:\Users\Kiera\OneDrive\Desktop\Programs\Made By Me\b1ueice.github.io\beta\proxykey.txt"
+
+    # Write the response text to both files
+    with open(file_path1, 'w') as file:
+        file.write(response.text)
     with open(file_path2, 'w') as file:
         file.write(response.text)
+
+    time.sleep(5)
     print("The script executed successfully. Committing to GitHub...")
 
     # Change directory to the Git repository
     os.chdir(r"C:\Users\Kiera\OneDrive\Desktop\Programs\Made By Me\b1ueice.github.io")
 
- 
+    # Git commands to add, commit, and push the changes
     try:
         subprocess.run(["git", "add", "proxykey.txt"], check=True)
-        subprocess.run(["git", "commit", "-m", "Update proxykey.txt"], check=True)
+        subprocess.run(["git", "add", r"beta\proxykey.txt"], check=True)
+        subprocess.run(["git", "commit", "-m", "Update proxykey.txt files"], check=True)
         subprocess.run(["git", "push"], check=True)
         print("Successfully committed to GitHub.")
     except subprocess.CalledProcessError as e:
@@ -32,4 +38,4 @@ try:
 except Exception as e:
     print(f"An error occurred: {e}")
 
-time.sleep(180)
+time.sleep(10)
